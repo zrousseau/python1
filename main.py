991a953480e871de8e6c056826b4f0125997c66a
zrousseau <zrousseau038@g.rwu.edu>
2021-09-19 19:17:47 -0400

    Add ask_name, multiply_number and print_letter_count functions

--- main.py
+++ main.py
@@ -3,16 +3,38 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+def ask_name():
+    # ask user for name
+    name = input("what is your name\n>")
+    # print user's name
+    print(f"Hello {name}")
+
+
+def multiply_number():
+    # ask the user for a number
+    num = float(input("Enter a number\n>"))
+    # multiply number by itself
+    print(f' {num} times {num} is {num*num}')
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def print_letter_count():
+    # ask the user for a word
+    word = input("Please enter a word\n")
+    # print the letter count
+    print("The length of the word is", len(word))
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Zach')
+    # print_hi('Zach')
 
-print('printy print')
-print("Goodbye, World!")
+    ask_name()
+    multiply_number()
+    print_letter_count()
+
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
