a7b1d3e3b762664ba90933b722796b748020f908
zrousseau <zrousseau038@g.rwu.edu>
2021-09-24 15:56:17 -0400

    Added coin_flip and array_flip

--- main.py
+++ main.py
@@ -28,13 +28,47 @@ def print_letter_count():
     # print the letter count
     print("The length of the word is", len(word))
 
+
+def array_flip():
+    array = [['.', '.', '.', '.', '.', '.'],
+             ['.', 'O', 'O', '.', '.', '.'],
+             ['O', 'O', 'O', 'O', '.', '.'],
+             ['O', 'O', 'O', 'O', 'O', '.'],
+             ['.', 'O', 'O', 'O', 'O', 'O'],
+             ['O', 'O', 'O', 'O', 'O', '.'],
+             ['O', 'O', 'O', 'O', '.', '.'],
+             ['.', 'O', 'O', '.', '.', '.'],
+             ['.', '.', '.', '.', '.', '.']]
+    for i in range(6):
+        for j in range(9):
+            print(array[j][i], end="")
+        print()
+
+
+def coin_flip():
+    number = int(input("Pick a number 0 or 1"))
+    one_or_two = [0,1]
+    if(random.choice(one_or_two) == number):
+        print("You win!")
+    else:
+        print("You lose...")
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    import random
     # print_hi('Zach')
 
-    ask_name()
-    multiply_number()
-    print_letter_count()
+    # ask_name()
+    # multiply_number()
+    # print_letter_count()
+    choice = input("Do you want to do the array flip (a) or coin flip (c)")
+    if(choice == "a"):
+        array_flip()
+    elif(choice == "c"):
+        coin_flip()
+    else:
+        print("Sorry that is not an option...")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
