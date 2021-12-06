b67c99e0dab5ef3d0cb105d6ae47c1bb52de4678
zrousseau <zrousseau038@g.rwu.edu>
2021-12-05 21:37:40 -0500

    Added branch for assignment1

--- main.py
+++ main.py
@@ -1,46 +1,15 @@
-class Bird:
-    species = 'bird'
-    beak_length = 3.2
+# This is a sample Python script.
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_hi('Zach')
 
-    def __init__(self,name):
-        self.name = name
-
-
-    def change_wingspan(self, wingspan):
-        self.wingspan = wingspan
-
-    def get_wingspan(self):
-        print("{}".format(self.wingspan))
-
-
-    def chirp(self):
-        print("Caw Caw I'm a widdle birdie called {}".format(self.name))
-
-
-    def __str__(self):
-        return print("The {},".format(self.species), "{}".format(self.name), "has a wingspan of {}".format(self.wingspan), "cm")
-
-
-a = Bird('')
-b = Bird('')
-
-name1 = input("What would you like to name the first bird?")
-name2 = input("What would you like to name the second bird")
-
-a.name = name1
-b.name = name2
-
-try:
-    a.change_wingspan(float(input("What is the wingspan of the first bird?")))
-    b.change_wingspan(float(input("What is the wingspan of the second bird?")))
-except:
-    print("The wingspan you entered is not an number, a wingspan of 30cm has been applied")
-    wingspan1 = 30
-    wingspan2 = 30
-
-
-
-print(a.__str__())
-print(b.__str__())
-print(help(a))
+print('printy print')
+print("Goodbye, World!")
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
