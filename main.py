75afa627d9a551269799e2184a3ecf0de6a21615
zrousseau <zrousseau038@g.rwu.edu>
2021-12-08 16:58:04 -0500

    Added branch tictac

--- main.py
+++ main.py
@@ -1,73 +1,3 @@
-from statistics import mean as MEAN
-import sys
-
-
-def singular(string):
-    if(string.endswith('s')):
-       return string[:-1]
-    else:
-        return string
-
-def titleTime(string):
-    string = string.title()
-    string = string.center(20, "=")
-    return string
-
-def findMean():
-    print("The mean is", MEAN([1, 2, 3, 4, 5, 6, 7]))
-
-
-def ask_name():
-    # ask user for name
-    name = input("what is your name\n>")
-    # print user's name
-    print(f"Hello {name}")
-
-
-def multiply_number():
-    # ask the user for a number
-    num = float(input("Enter a number\n>"))
-    # multiply number by itself
-    print(f' {num} times {num} is {num*num}')
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def print_letter_count():
-    # ask the user for a word
-    word = input("Please enter a word\n")
-    # print the letter count
-    print("The length of the word is", len(word))
-
-
-def array_flip():
-    array = [['.', '.', '.', '.', '.', '.'],
-             ['.', 'O', 'O', '.', '.', '.'],
-             ['O', 'O', 'O', 'O', '.', '.'],
-             ['O', 'O', 'O', 'O', 'O', '.'],
-             ['.', 'O', 'O', 'O', 'O', 'O'],
-             ['O', 'O', 'O', 'O', 'O', '.'],
-             ['O', 'O', 'O', 'O', '.', '.'],
-             ['.', 'O', 'O', '.', '.', '.'],
-             ['.', '.', '.', '.', '.', '.']]
-    for i in range(6):
-        for j in range(9):
-            print(array[j][i], end="")
-        print()
-
-
-def coin_flip():
-    number = int(input("Pick a number 0 or 1"))
-    one_or_two = [0,1]
-    if(random.choice(one_or_two) == number):
-        print("You win!")
-    else:
-        print("You lose...")
-
-# a
 # Tic Tac Toe Time
 import random
 
@@ -104,7 +34,7 @@ def enter_move(board):
         for i in range(3):
             for j in range(3):
                 if(move == board[i][j]):
-                    # grid[i][j] = 'O'
+                    grid[i][j] = 'O'
                     return board
         print("That is not a free move")
         continue
@@ -116,12 +46,12 @@ def make_list_of_free_fields(board):
     # The function browses the board and builds a list of all the free squares;
     # the list consists of tuples, while each tuple is a pair of row and column numbers.
     print("Making list")
-    # free_squares.clear()
-    # for i in range(3):
-        # for j in range(3):
-            # if(board[i][j] != 'O' and board[i][j] != 'X'):
-                    # free_squares.append(tuple((i,j)))
-    # return free_squares
+    free_squares.clear()
+    for i in range(3):
+        for j in range(3):
+            if(board[i][j] != 'O' and board[i][j] != 'X'):
+                free_squares.append(tuple((i,j)))
+    return free_squares
 
 
 def victory_for(board, sign):
@@ -154,8 +84,8 @@ def draw_move(board):
         move = random.randrange(1,10)
         for i in range(3):
             for j in range(3):
-                # if (move == board[i][j]):
-                    # grid[i][j] = 'X'
+                if (move == board[i][j]):
+                    grid[i][j] = 'X'
                     return board
         continue
 
@@ -167,6 +97,7 @@ if __name__ == '__main__':
     # ask_name()
     # multiply_number()
     # print_letter_count()
+
     # choice = input("Do you want to do the array flip (a) or coin flip (c)")
     # if(choice == "a"):
     #   array_flip()
@@ -175,32 +106,23 @@ if __name__ == '__main__':
     # else:
     #    print("Sorry that is not an option...")
     # Tic Tac Continued
-    # grid = [[1, 2, 3, ],
-    #        [4, 'X', 6],
-    #        [7, 8, 9]]
-    # free_squares = []
-    # while(True):
-    #    make_list_of_free_fields(grid)
-    #    if (len(free_squares) == 0):
-    #        display_board(grid)
-    #        print("The game is a draw")
-    #        break
-    #    display_board(grid)
-    #    enter_move(grid)
-    #    if(victory_for(grid, 'O')):
-    #       print('You have won!')
-    #        break
-    #    draw_move(grid)
-    #    if(victory_for(grid, 'X')):
-    #        display_board(grid)
-    #        print('The computer has won.')
-    #        break
-    # findMean()
-    print(str(sys.argv))
-    print(singular(sys.argv[1]))
-    print(titleTime(" ".join(sys.argv[1:])))
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    grid = [[1, 2, 3, ],
+            [4, 'X', 6],
+            [7, 8, 9]]
+    free_squares = []
+    while(True):
+        make_list_of_free_fields(grid)
+        if (len(free_squares) == 0):
+            display_board(grid)
+            print("The game is a draw")
+            break
+        display_board(grid)
+        enter_move(grid)
+        if(victory_for(grid, 'O')):
+            print('You have won!')
+            break
+        draw_move(grid)
+        if(victory_for(grid, 'X')):
+            display_board(grid)
+            print('The computer has won.')
+            break
